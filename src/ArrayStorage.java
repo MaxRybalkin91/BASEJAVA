e9bae8b6dae9010c410357cf9325396a19809e74
Maksim Rybalkin <granirock@gmail.com>
2019-04-07 10:44:20 +0300

    Add equals() in get() and delete()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -19,7 +19,7 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         for(int i = 0; i < size; i++) {
-            if(storage[i].uuid == uuid) {
+            if(storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
         }
@@ -28,7 +28,7 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
+            if (storage[i].uuid.equals(uuid)) {
                 storage[i] = storage[size - 1];
                 size--;
             }
