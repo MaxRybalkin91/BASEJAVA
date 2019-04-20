dc7b69e3a655fc1de8710ac406fe7644275c6335
Maksim Rybalkin <granirock@gmail.com>
2019-04-20 21:13:34 +0300

    Add classes from lesson3

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -4,7 +4,7 @@ import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
 
-public class SortedArrayStorage extends AbstractArrayStorage {
+public class SortedArrayStorage extends AbstractArrayStorage{
     @Override
     public void clear() {
 
