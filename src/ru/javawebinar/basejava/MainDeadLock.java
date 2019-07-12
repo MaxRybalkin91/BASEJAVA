5426ae66eebeeb8f75d130f18fb08b87f5515350
Maksim Rybalkin <granirock@gmail.com>
2019-07-13 01:47:59 +0300

    HW11. Realization of "DeadLock"

---
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.basejava;
+
+public class MainDeadLock {
+    private static final Object OBJECT_1 = new Object();
+    private static final Object OBJECT_2 = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            synchronized (OBJECT_1) {
+                System.out.println("Waiting for OBJECT2 will be unlocked");
+                synchronized (OBJECT_2) {
+                    System.out.println("OBJECT2 is unlocked");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (OBJECT_2) {
+                System.out.println("Waiting for OBJECT1 will be unlocked");
+                synchronized (OBJECT_1) {
+                    System.out.println("OBJECT1 is unlocked");
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
