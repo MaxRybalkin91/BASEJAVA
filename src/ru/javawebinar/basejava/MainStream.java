a12fea53c04b0ada175a886307b6a25571775e7f
Maksim Rybalkin <granirock@gmail.com>
2019-07-22 19:31:29 +0300

    HW12. Task #1; realization of array (fix)

--- src/ru/javawebinar/basejava/MainStream.java
+++ src/ru/javawebinar/basejava/MainStream.java
@@ -7,23 +7,21 @@ import java.util.stream.IntStream;
 public class MainStream {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number;
+
         System.out.println("Enter the desired array size:");
         int[] array = new int[scanner.nextInt()];
         for (int i = 0; i < array.length; i++) {
-            System.out.println("Enter " + (i + 1) + " number:");
-            array[i] = scanner.nextInt();
+            do {
+                System.out.println("Enter " + (i + 1) + " number (required in the range of 1 to 9):");
+                number = scanner.nextInt();
+                array[i] = number;
+            } while (number < 1 || number > 9);
         }
-        System.out.println(minValue(array));
+        System.out.print("There is number from the unique parts of your array:" + "\n" + minValue(array));
     }
 
     private static int minValue(int[] array) {
-        int[] noDuplicates = IntStream.of(array).distinct().toArray();
-        int result = 0;
-
-        for (int i = noDuplicates.length - 1, n = 0; i >= 0; --i, n++) {
-            int pos = (int) Math.pow(10, i);
-            result += noDuplicates[n] * pos;
-        }
-        return result;
+        return IntStream.of(array).distinct().reduce(0, (a, b) -> 10 * a + b);
     }
 }
