75531383552e1213662370c5833e1e7968dea0bc
Maksim Rybalkin <granirock@gmail.com>
2019-07-23 00:17:47 +0300

    HW12. Task #2; realization of list. Fix task #1

--- src/ru/javawebinar/basejava/MainStream.java
+++ src/ru/javawebinar/basejava/MainStream.java
@@ -1,12 +1,31 @@
 package ru.javawebinar.basejava;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 
 public class MainStream {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int answer;
+
+        System.out.println("Choose the programm:");
+        do {
+            System.out.println("1 - array, 2 - list");
+            answer = scanner.nextInt();
+        } while (answer != 1 && answer != 2);
+
+        if (answer == 1) {
+            deleteDuplicatesFromArray(scanner);
+        } else {
+            deleteNumbers(scanner);
+        }
+    }
+
+    private static void deleteDuplicatesFromArray(Scanner scanner) {
         int number;
 
         System.out.println("Enter the desired array size:");
@@ -18,10 +37,34 @@ public class MainStream {
             } while (number < 1 || number > 9);
             array[i] = number;
         }
-        System.out.print("There is number from the unique parts of your array:" + "\n" + minValue(array));
+        System.out.println("There is number from the unique parts of your array:" + "\n" + minValue(array));
+    }
+
+    private static void deleteNumbers(Scanner scanner) {
+        List<Integer> integers = new ArrayList<>();
+
+        System.out.println("Enter the numbers for the list (enter any letter to stop):");
+        while (scanner.hasNextInt()) {
+            integers.add(scanner.nextInt());
+        }
+
+        for (int n : oddOrEven(integers)) {
+            System.out.print(n + " ");
+        }
     }
 
     private static int minValue(int[] array) {
-        return IntStream.of(array).distinct().reduce(0, (a, b) -> 10 * a + b);
+        return IntStream.of(array)
+                .distinct()
+                .reduce(0, (a, b) -> 10 * a + b);
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        return integers.stream()
+                .reduce(0, Integer::sum) == 0 ?
+                integers.stream()
+                        .filter(p -> p % 2 == 0).collect(Collectors.toList()) :
+                integers.stream()
+                        .filter(p -> p % 2 != 0).collect(Collectors.toList());
     }
 }
